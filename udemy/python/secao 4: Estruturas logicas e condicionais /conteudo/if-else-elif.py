33ba6d3b129ab20dd1563bf740c4b72785d932a0
Alberto-Leal <alberto.sanctum@gmail.com>
2024-06-22 16:01:53 -0300

    Atualizando if

---
+++ udemy/python/secao 4: Estruturas logicas e condicionais /conteudo/if-else-elif.py
@@ -0,0 +1,13 @@
+""".
+Estruturas condicionais - 
+    São blocos de códigos que ALTERNAM situações pra desencadear
+"""
+
+
+"""
+if(se)
+"""
+idade = 16
+
+if idade < 18:
+    print('MENOR DE IDADE')
