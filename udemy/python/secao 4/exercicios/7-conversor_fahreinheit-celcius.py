d9f28c3b8bc9daaf4af1ea2cd812305ee95ecb53
Alberto-Leal <alberto.sanctum@gmail.com>
2024-06-22 14:22:27 -0300

    Add exercício 7

---
+++ udemy/python/secao 4/exercicios/7-conversor_fahreinheit-celcius.py
@@ -0,0 +1,12 @@
+
+"""
+Converta uma temperatura de fahreinheit para celcius e exiba na tela.
+A fómula de conversão é "C = 5.0 * (F - 32.0) / 9.0".
+"""
+
+F = 78;
+
+C = 5.0 * (F - 32.0) / 9.0;
+
+
+print(C);
