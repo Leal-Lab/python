2b8257378751c9ddb32b3f5c24e163b0a73132ee
Alberto Leal <alberto.sanctum@gmail.com>
2024-08-01 01:42:05 -0300

    secao 5

---
+++ 1-curso/secao 5: Estruturas de repeticao/conteudo/aula1_for.py
@@ -0,0 +1,9 @@
+"""
+For é uma estrutura que percorre o array e executa alguma coisa. Ele é capaz de criar uma variável e atribuir valor 
+(ainda que temporário) a ela.
+"""
+
+LISTA_COMPRAS = ['morango', 'beterraba', 'laranja']
+
+for item in LISTA_COMPRAS:
+    print (item)
