1b8d40efdb79aaf15a978b5ffd8fcab70a852862
Alberto Leal <alberto.sanctum@gmail.com>
2024-09-06 03:05:32 -0300

    Resolvendo exercício 3 - secão 5

---
+++ Udemy/secao 5: Estruturas de repeticao/exercicios/ex3_add_1000.py
@@ -0,0 +1,10 @@
+"""
+3. Faça um programa que declare um inteiro, inicialize-o com 0, incremente-o de 1000 em 1000, imprimindo
+seu valor na tela, até que seu valor seja 100000 (cem mil).
+"""
+
+CONTADOR = 0
+
+while CONTADOR <= 100000:
+    print(CONTADOR)
+    CONTADOR = CONTADOR + 1000
