8d91ded56e581da9c7c3335e2ba8daa30a68ea8d
Alberto-Leal <alberto.sanctum@gmail.com>
2025-08-12 14:42:02 -0300

    Iniciando o projeto

---
+++ Projetos/DIO/02: Criando um Sistema Bancário com Python/my_bank.py
@@ -0,0 +1,12 @@
+'''
+ Criar um sistema bancário com as operações:
+    sacar, depositar e visualizar extrato.
+'''
+
+deposito = input("Digite o valor a depositar: ")
+
+saque = input('Digite o valor a sacar: ')
+
+extrato = print(deposito - saque)
+
+
